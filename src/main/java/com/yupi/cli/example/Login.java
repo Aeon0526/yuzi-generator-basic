ce0523240edf8fa967b7492b3d4aa201a033b00b
Sun <1176550817@qq.com>
2024-02-11 23:03:48 +0800

    可选交互式输入

--- src/main/java/com/yupi/cli/example/Login.java
+++ src/main/java/com/yupi/cli/example/Login.java
@@ -9,16 +9,12 @@ public class Login implements Callable<Integer> {
 	@Option(names = {"-u", "--user"}, description = "User name")
 	String user;
 
-//	  // Picocli 默认输入密码
-//    @Option(names = {"-p", "--password"}, description = "Passphrase", interactive = true)
-//    String password;
-
 	// 修改输入密码提示
-	@Option(names = {"-p", "--password"}, description = "Passphrase", interactive = true, echo = false, prompt = "请输入密码：")
+	@Option(names = {"-p", "--password"}, description = "Passphrase",arity = "0..1",  interactive = true, echo = false, prompt = "请输入密码：")
 	String password;
 
 	// 接收校验密码
-	@Option(names = {"-cp", "--checkPassword"}, description = "Check Password", interactive = true, prompt = "请校验密码：")
+	@Option(names = {"-cp", "--checkPassword"}, description = "Check Password",arity = "0..1",  interactive = true, prompt = "请校验密码：")
 	String checkPassword;
 
 	public Integer call() throws Exception {
@@ -28,7 +24,6 @@ public class Login implements Callable<Integer> {
 	}
 
 	public static void main(String[] args) {
-//		new CommandLine(new Login()).execute("-u", "user123", "-p");	// 只有输入密码，没有校验
-		new CommandLine(new Login()).execute("-u", "user123", "-p", "-cp");// 输入密码，并校验
+		new CommandLine(new Login()).execute("-u", "user123", "-p", "xxx", "-cp", "xxx");// 输入密码，并校验
 	}
 }
