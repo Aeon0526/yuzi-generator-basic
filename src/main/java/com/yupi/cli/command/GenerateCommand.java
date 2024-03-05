5603c6130c6b365490966863d9095dfa12fa1f2c
Sun <1176550817@qq.com>
2024-03-05 22:46:37 +0800

    Picocli_子命令实现_generate 子命令

--- src/main/java/com/yupi/cli/command/GenerateCommand.java
+++ src/main/java/com/yupi/cli/command/GenerateCommand.java
@@ -1,4 +1,37 @@
 package com.yupi.cli.command;
 
-public class GenerateCommand {
+import cn.hutool.core.bean.BeanUtil;
+import com.yupi.generator.MainGenerator;
+import com.yupi.model.MainTemplateConfig;
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "generate", mixinStandardHelpOptions = true)
+public class GenerateCommand implements Callable<Integer> {
+	/**
+	 * 是否生成循环(开关)
+	 */
+	@CommandLine.Option(names = {"-l", "--loop"}, description = "是否循环",arity = "0..1",  interactive = true)
+	private boolean loop;
+
+	/**
+	 * 作者注释(字符串填充值)
+	 */
+	@CommandLine.Option(names = {"-a", "--author"}, description = "作者名称",arity = "0..1",  interactive = true)
+	private String author = "yupi";
+
+	/**
+	 * 输出信息
+	 */
+	@CommandLine.Option(names = {"-o", "--outputText"}, description = "输出文本",arity = "0..1",  interactive = true)
+	private String outputText = "sum = ";
+
+	@Override
+	public Integer call() throws Exception {
+		MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+		BeanUtil.copyProperties(this,mainTemplateConfig);
+		MainGenerator.doGenerate(mainTemplateConfig);
+		return 0;
+	}
 }
