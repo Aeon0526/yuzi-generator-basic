8fab34266820f20c7041918793de26f44a4ede4a
Sun <1176550817@qq.com>
2024-03-06 15:21:12 +0800

    Picocli_jar包构建及测试使用

--- src/main/java/com/yupi/cli/command/ConfigCommand.java
+++ src/main/java/com/yupi/cli/command/ConfigCommand.java
@@ -1,4 +1,22 @@
 package com.yupi.cli.command;
 
-public class ConfigCommand {
+import cn.hutool.core.util.ReflectUtil;
+import com.yupi.model.MainTemplateConfig;
+import picocli.CommandLine;
+
+import java.lang.reflect.Field;
+
+@CommandLine.Command(name = "config", mixinStandardHelpOptions = true)
+public class ConfigCommand implements Runnable{
+	@Override
+	public void run(){
+		// 获取MainTemplateConfig类的所有字段
+		Field[] fields = ReflectUtil.getFields(MainTemplateConfig.class);
+		// 遍历并打印每个字段的信息
+		for(Field field : fields){
+			System.out.println("字段类型"+field.getType());
+			System.out.println("字段名称"+field.getName());
+			System.out.println("---");
+		}
+	}
 }
--- src/main/java/com/yupi/cli/command/GenerateCommand.java
+++ src/main/java/com/yupi/cli/command/GenerateCommand.java
@@ -3,34 +3,37 @@ package com.yupi.cli.command;
 import cn.hutool.core.bean.BeanUtil;
 import com.yupi.generator.MainGenerator;
 import com.yupi.model.MainTemplateConfig;
+import lombok.Data;
 import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "generate", mixinStandardHelpOptions = true)
+@Data
 public class GenerateCommand implements Callable<Integer> {
 	/**
 	 * 是否生成循环(开关)
 	 */
-	@CommandLine.Option(names = {"-l", "--loop"}, description = "是否循环",arity = "0..1",  interactive = true)
+	@CommandLine.Option(names = {"-l", "--loop"}, description = "是否循环", arity = "0..1", interactive = true, echo = true)
 	private boolean loop;
 
 	/**
 	 * 作者注释(字符串填充值)
 	 */
-	@CommandLine.Option(names = {"-a", "--author"}, description = "作者名称",arity = "0..1",  interactive = true)
+	@CommandLine.Option(names = {"-a", "--author"}, description = "作者名称", arity = "0..1", interactive = true, echo = true)
 	private String author = "yupi";
 
 	/**
 	 * 输出信息
 	 */
-	@CommandLine.Option(names = {"-o", "--outputText"}, description = "输出文本",arity = "0..1",  interactive = true)
+	@CommandLine.Option(names = {"-o", "--outputText"}, description = "输出文本", arity = "0..1", interactive = true, echo = true)
 	private String outputText = "sum = ";
 
 	@Override
 	public Integer call() throws Exception {
 		MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
-		BeanUtil.copyProperties(this,mainTemplateConfig);
+		BeanUtil.copyProperties(this, mainTemplateConfig);
+		System.out.println("配置信息：" + mainTemplateConfig);
 		MainGenerator.doGenerate(mainTemplateConfig);
 		return 0;
 	}
